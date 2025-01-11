a165eda62a8acdb79ab9724d23ceda8bf55cf634
Hiroi Lika <wvouov.22.vouovw@gmail.com>
2025-01-11 23:59:50 +0900

    スタンダード第10回課題：StudentsとStudentsCourseのDB全件検索

---
+++ src/main/java/raisetech/StudentManagement/Students.java
@@ -0,0 +1,19 @@
+package raisetech.StudentManagement;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Students {
+  //必須：studentId, fullName, fullNameKana
+  //studentId:PRIMARY KEY
+  private String studentId;
+  private String fullName;
+  private String fullNameKana;
+  private String nickname;
+  private String mailAddress;
+  private String residenceArea;
+  private Integer age;
+  private String sex;
+}
