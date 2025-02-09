9eee7321814f275871160b42a8532fd1218e73cc
Hiroi Lika <wvouov.22.vouovw@gmail.com>
2025-02-09 15:38:28 +0900

    修正：各テーブルのid自動生成での登録・serviceの処理コード簡素化

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,9 +1,9 @@
 package raisetech.StudentManagement.repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
@@ -25,15 +25,13 @@ public interface StudentRepository {
       "INSERT INTO students (student_id, full_name, full_name_kana, nickname, mail_address, residence_area, age, sex, remark) "
           +
           "VALUES (#{studentId}, #{fullName}, #{fullNameKana}, #{nickname}, #{mailAddress}, #{residenceArea}, #{age}, #{sex}, #{remark})")
-  void registerStudent(String studentId, String fullName, String fullNameKana, String nickname,
-      String mailAddress, String residenceArea, Integer age, String sex, String remark);
+  @Options(useGeneratedKeys = true, keyProperty = "studentId")
+  void registerStudent(Student student);
 
   @Insert(
       "INSERT INTO students_courses (course_id, student_id, course, start_date, end_date) "
           +
           "VALUES (#{courseId}, #{studentId}, #{course}, #{startDate}, #{endDate})")
-  void registerStudentCourse(String courseId, String studentId, String course,
-      LocalDate startDate, LocalDate endDate);
-
-
+  @Options(useGeneratedKeys = true, keyProperty = "courseId")
+  void registerStudentCourse(StudentsCourses studentsCourses);
 }
--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -1,10 +1,9 @@
 package raisetech.StudentManagement.service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
@@ -29,52 +28,25 @@ public class StudentService {
     return repository.searchStudentsCourses();
   }
 
-  //studentIDの発行("S+YYMMDD+その日の登録数連番"で構成　YYMMDDは西暦年/月/日)
-  public String createStudentId() {
-    //YYMMDDの作成
-    LocalDate today = LocalDate.now();
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
-    String todayFormatted = today.format(formatter);
-    //登録数連番の最終番号を取得し新規登録に割り当てるべき連番を設定
-    List<Student> students = repository.searchStudents();
-    Integer lastRegisterCount = 0;
-    for (Student student : students) {
-      String datePart = student.getStudentId().substring(1, 7);
-      String countPart = student.getStudentId().substring(7, 10);
-      Integer registerCount = Integer.parseInt(countPart);
-      if (datePart.equals(todayFormatted) && lastRegisterCount < registerCount) {
-        lastRegisterCount = registerCount;
-      }
-    }
-    String lastRegisterCountString = String.format("%03d", lastRegisterCount + 1);
-
-    return "S" + todayFormatted + lastRegisterCountString;
-  }
-
-  // 新規生徒情報登録 (DB:studentsへの登録)
+  // 新規生徒情報登録 (DB:studentsへの登録　補：courseの入力がある場合はstudents_coursesも登録)
+  @Transactional
   public void registerStudent(StudentDetail studentDetail) {
-    String studentId = createStudentId();
+    repository.registerStudent(studentDetail.getStudent());
 
-    repository.registerStudent(studentId, studentDetail.getStudent().getFullName(),
-        studentDetail.getStudent().getFullNameKana(), studentDetail.getStudent().getNickname(),
-        studentDetail.getStudent().getMailAddress(), studentDetail.getStudent().getResidenceArea(),
-        studentDetail.getStudent().getAge(), studentDetail.getStudent().getSex(),
-        studentDetail.getStudent().getRemark());
-
-    //コース情報が入力されている場合はコースを登録
-    if (!studentDetail.getStudentsCourses().getFirst().getCourseId().isEmpty()) {
-      studentDetail.getStudentsCourses().getFirst().setStudentId(studentId);
+    //コース名が入力されている場合はコースを登録
+    if (!studentDetail.getStudentsCourses().getFirst().getCourse().isEmpty()) {
+      studentDetail.getStudentsCourses().forEach(sc ->
+          sc.setStudentId(studentDetail.getStudent().getStudentId())
+      );
       registerStudentCourse(studentDetail);
     }
-
   }
 
+  //コース登録
+  @Transactional
   public void registerStudentCourse(StudentDetail studentDetail) {
-    repository.registerStudentCourse(studentDetail.getStudentsCourses().getFirst().getCourseId(),
-        studentDetail.getStudentsCourses().getFirst().getStudentId(),
-        studentDetail.getStudentsCourses().getFirst().getCourse(),
-        studentDetail.getStudentsCourses().getFirst().getStartDate(),
-        studentDetail.getStudentsCourses().getFirst().getEndDate());
+    for (StudentsCourses studentsCourses : studentDetail.getStudentsCourses()) {
+      repository.registerStudentCourse(studentsCourses);
+    }
   }
-
 }
