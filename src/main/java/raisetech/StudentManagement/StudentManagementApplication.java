f68314ceaddfa1c066d942d535071cdccf7bc47e
Lika <wvouov.22.vouovw@gmail.com>
2025-05-22 19:37:14 +0900

    バージョン管理外ファイルの追加

---
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -0,0 +1,21 @@
+package raisetech.StudentManagement;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+	@GetMapping("/helloWorld")
+	public String hello(){
+		return "Hello world!";
+	}
+}
