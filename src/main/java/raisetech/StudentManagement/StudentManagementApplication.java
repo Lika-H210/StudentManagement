c105582e9b08baa2be509890d030702067d67760
Lika <wvouov.22.vouovw@gmail.com>
2025-05-24 00:32:13 +0900

    Map<key,受講生情報>のGET/POST/PUT/PATCH処理の実装

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,21 +1,79 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  private String name = "Enami Koji";
+  private Integer age = 30;
+  private Map<Integer, Student> student = new HashMap<>();
+
+  //初期はMapに1件のデータが入っている。
+  {
+    student.put(1, new Student("Enami Koji", 30));
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/helloWorld")
-  public String hello() {
-    return "Hello world!";
+  @GetMapping("/studentInfo")
+  public Map<Integer, Student> getStudentInfo() {
+    return student;
+  }
+
+  //新規受講生の登録
+  @PostMapping("/studentInfo")
+  public void addStudentInfo(String name, Integer age) {
+    //キーを作成
+    int newKey = student.keySet().stream()
+        .mapToInt(Integer::intValue)
+        .max()
+        .orElse(0) + 1;
+    Student newStudent = new Student(name, age);
+    student.put(newKey, newStudent);
+  }
+
+  //引数Studentで登録処理ver.
+  @PostMapping("/studentInfoByStudent")
+  public void addStudentInfoByStudent(Student newStudent) {
+    //キーを作成
+    int newKey = student.keySet().stream()
+        .mapToInt(Integer::intValue)
+        .max()
+        .orElse(0) + 1;
+    student.put(newKey, newStudent);
+  }
+
+  //keyを参照し対象の受講生情報を更新
+  @PutMapping("/studentInfo")
+  public void putUpdateStudentInfo(Integer key, String name, Integer age) {
+    Student target = student.get(key);
+    if (target != null) {
+      target.setName(name);
+      target.setAge(age);
+    }
+  }
+
+  //keyを参照し対象の受講生情報を部分更新
+  @PatchMapping("/studentInfo")
+  public void patchUpdateStudentInfo(Integer key, String name, Integer age) {
+    Student target = student.get(key);
+    if (target != null && name != null) {
+      target.setName(name);
+    }
+    if (target != null && age != null) {
+      target.setAge(age);
+    }
   }
 }
