879d7ffc66d39bbaf0cf6fafaf50217dfdf3c658
Lika <wvouov.22.vouovw@gmail.com>
2025-06-02 21:30:24 +0900

    Thymeleafでの画面描画処理の実装

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -2,16 +2,17 @@ package raisetech.StudentManagement.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.date.Student;
 import raisetech.StudentManagement.date.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -24,10 +25,13 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> studentList = service.searchStudentList();
     List<StudentCourse> studentCourseList = service.searchStudentCourseList();
-    return converter.convertToStudentDetail(studentList, studentCourseList);
+    List<StudentDetail> studentDetailList = converter.convertToStudentDetail(studentList,
+        studentCourseList);
+    model.addAttribute("studentList", studentDetailList);
+    return "studentList";
   }
 
   //todo:引数をpublicIdに変更する
