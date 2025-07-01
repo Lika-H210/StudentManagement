6514efdff895fe820e3e03cc84344d800f639bc4
Lika <wvouov.22.vouovw@gmail.com>
2025-07-01 17:14:37 +0900

    Serviceテスト内容の適正化

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -1,16 +1,14 @@
 package raisetech.StudentManagement.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -19,18 +17,14 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
 import raisetech.StudentManagement.domain.StudentDetail;
-import raisetech.StudentManagement.exception.custom.IllegalResourceAccessException;
 import raisetech.StudentManagement.exception.custom.NotUniqueException;
 import raisetech.StudentManagement.repository.StudentRepository;
 import raisetech.StudentManagement.service.converter.StudentConverter;
@@ -89,24 +83,6 @@ class StudentServiceTest {
     assertEquals(expected, actual);
   }
 
-  //受講生個人検索：例外系(404：IllegalResourceAccessException:未登録publicIDで受講生を検索した場合)
-  @Test
-  void 受講生詳細個人取得処理で未登録publicIdで検索した場合に例外が生じ処理が中断されること() {
-    String publicId = "00000000-0000-0000-0000-000000000000";
-
-    when(repository.searchStudentByPublicId(publicId)).thenReturn(null);
-
-    IllegalResourceAccessException thrown = assertThrows(
-        IllegalResourceAccessException.class,
-        () -> sut.searchStudentDetailByPublicId(publicId)
-    );
-    assertEquals("受講生情報の取得中に問題が発生しました。システム管理者までご連絡ください。",
-        thrown.getMessage());
-    verify(repository, times(1)).searchStudentByPublicId(publicId);
-    verify(repository, never()).searchStudentCourseListByStudentId(anyInt());
-  }
-
-
   //受講生登録処理：正常系
   @Test
   void 受講生詳細登録処理でrepositoryとクラス内メソッドの呼び出し及びpublicIdの付与が適切に行われていること()
@@ -114,7 +90,8 @@ class StudentServiceTest {
     Integer studentId = 999;
     String email = "test@example.com";
     student.setEmail(email);
-    StudentDetail studentDetail = new StudentDetail(student, new ArrayList<>());
+    StudentCourse studentCourse = new StudentCourse();
+    StudentDetail studentDetail = new StudentDetail(student, List.of(studentCourse));
 
     when(repository.existsByEmail(email)).thenReturn(false);
     doAnswer(invocation -> {
@@ -123,14 +100,11 @@ class StudentServiceTest {
       return null;
     }).when(repository).registerStudent(studentDetail.getStudent());
 
-    StudentService spyService = spy(sut);
-
-    StudentDetail actual = spyService.registerStudentDetail(studentDetail);
+    StudentDetail actual = sut.registerStudentDetail(studentDetail);
 
     verify(repository, times(1)).existsByEmail(email);
     verify(repository, times(1)).registerStudent(student);
-    verify(spyService, times(1)).registerStudentCourse(studentId,
-        studentDetail.getStudentCourseList());
+    verify(repository, times(1)).registerStudentCourse(studentCourse);
     assertNotNull(actual.getStudent().getPublicId());
     assertDoesNotThrow(() -> UUID.fromString(actual.getStudent().getPublicId()));
   }
@@ -149,25 +123,36 @@ class StudentServiceTest {
         () -> sut.registerStudentDetail(studentDetail)
     );
     assertEquals("このメールアドレスは既に登録されています", thrown.getMessage());
-    verify(repository, times(1)).existsByEmail(email);
     verify(repository, never()).registerStudent(any(Student.class));
   }
 
-  //受講コース登録処理：正常系
-  @ParameterizedTest
-  @MethodSource("courseListTestCase")
-  void 受講コースの登録でコース登録ripository及び初期値設定メソッドがコースリストサイズ分だけ呼び出されていること(
-      List<StudentCourse> studentCourseList) {
+  //受講コース登録処理：正常系(複数コース登録）
+  @Test
+  void 受講コースの登録で複数コース登録時にripositoryとコース情報初期設定メソッドが登録回数分呼び出されていること() {
     Integer studentId = 999;
+    List<StudentCourse> studentCourseList = List.of(new StudentCourse(), new StudentCourse());
 
-    StudentService spyService = spy(sut);
-
-    spyService.registerStudentCourse(studentId, studentCourseList);
+    sut.registerStudentCourse(studentId, studentCourseList);
 
     verify(repository, times(studentCourseList.size())).registerStudentCourse(
         any(StudentCourse.class));
-    verify(spyService, times(studentCourseList.size())).initializeStudentCourse(eq(studentId),
-        any(StudentCourse.class));
+    assertThat(studentCourseList)
+        .hasSize(studentCourseList.size())
+        .allSatisfy(course -> {
+          assertThat(course.getStudentId())
+              .isEqualTo(studentId);
+        });
+  }
+
+  //受講コース登録処理：正常系(コース情報 空)
+  @Test
+  void 受講コースの登録でコースリストが空の時にripositoryは呼び出されないこと() {
+    Integer studentId = 999;
+    List<StudentCourse> studentCourseList = List.of();
+
+    sut.registerStudentCourse(studentId, studentCourseList);
+
+    verify(repository, never()).registerStudentCourse(any(StudentCourse.class));
   }
 
   //受講コース登録処理：登録項目の初期値設定(StudentCourseがstartDateを含む場合)
@@ -186,25 +171,24 @@ class StudentServiceTest {
 
   //受講コース登録処理：登録項目の初期値設定(StudentCourseのstartDateがnullの場合)
   @Test
-  void 受講コース情報初期値としてstudentIdがstudentCourseに反映されendDateはnullであること() {
+  void 受講コース情報初期値としてstartDateがnullの場合はendDateもnullであること() {
     Integer studentId = 999;
     StudentCourse studentCourse = new StudentCourse();
 
     sut.initializeStudentCourse(studentId, studentCourse);
 
-    assertEquals(studentId, studentCourse.getStudentId());
     assertNull(studentCourse.getEndDate());
   }
 
-  //受講生更新処理：正常系
-  @ParameterizedTest
-  @MethodSource("courseListTestCase")
-  void 受講生詳細情報の更新処理で必要なrepositoryが適切な回数だけ呼び出されていること(
-      List<StudentCourse> studentCourseList) throws NotUniqueException {
+  //受講生更新処理：正常系(複数コース更新）
+  @Test
+  void 受講生詳細情報の更新処理で必要なrepositoryが適切な回数だけ呼び出されていること()
+      throws NotUniqueException {
     String publicId = "00000000-0000-0000-0000-000000000000";
     String email = "test@example.com";
     student.setEmail(email);
     student.setPublicId(publicId);
+    List<StudentCourse> studentCourseList = List.of(new StudentCourse(), new StudentCourse());
     StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
 
     when(repository.searchStudentByPublicId(publicId)).thenReturn(student);
@@ -220,26 +204,22 @@ class StudentServiceTest {
         any(StudentCourse.class));
   }
 
-  //受講生更新処理：例外系(404:IllegalResourceAccessException:未登録のpublicIDで更新処理を実行した場合)
+  //受講生更新処理：正常系(更新コースリスト 空）
   @Test
-  void 受講生更新処理で未登録publicIdを参照し更新した場合に例外が生じ処理が中断されること() {
+  void 受講生詳細情報の更新処理で受講コース情報のrepositoryが実行されないこと()
+      throws NotUniqueException {
     String publicId = "00000000-0000-0000-0000-000000000000";
     String email = "test@example.com";
     student.setEmail(email);
     student.setPublicId(publicId);
-    StudentDetail studentDetail = new StudentDetail(student, new ArrayList<>());
+    StudentDetail studentDetail = new StudentDetail(student, List.of());
 
-    when(repository.searchStudentByPublicId(publicId)).thenReturn(null);
+    when(repository.searchStudentByPublicId(publicId)).thenReturn(student);
+    when(repository.existsByEmailExcludingPublicId(publicId, email)).thenReturn(false);
 
-    IllegalResourceAccessException thrown = assertThrows(
-        IllegalResourceAccessException.class,
-        () -> sut.updateStudentDetail(studentDetail));
+    sut.updateStudentDetail(studentDetail);
 
-    assertEquals("受講生情報の取得中に問題が発生しました。システム管理者までご連絡ください。",
-        thrown.getMessage());
-    verify(repository, times(1)).searchStudentByPublicId(publicId);
-    verify(repository, never()).existsByEmailExcludingPublicId(publicId, email);
-    verify(repository, never()).updateStudent(student);
+    verify(repository, never()).updateStudentCourse(any(StudentCourse.class));
   }
 
   //受講生更新処理：例外系(400:NotUniqueException:登録済みemailで更新処理した場合)
@@ -258,20 +238,8 @@ class StudentServiceTest {
         NotUniqueException.class,
         () -> sut.updateStudentDetail(studentDetail)
     );
-
     assertEquals("このメールアドレスは使用できません。", thrown.getMessage());
-    verify(repository, times(1)).searchStudentByPublicId(publicId);
-    verify(repository, times(1))
-        .existsByEmailExcludingPublicId(publicId, email);
     verify(repository, never()).updateStudent(student);
   }
 
-  //コース登録・更新処理でStudentCourseListの情報の有or空によらず正常に処理が行えるか確認するためのテストケース
-  // (StudentDetailのstudentCourseList=nullはバリデーションで除外されるため検査対象外としました）
-  private static Stream<List<StudentCourse>> courseListTestCase() {
-    return Stream.of(
-        List.of(new StudentCourse(), new StudentCourse()),
-        List.of()
-    );
-  }
 }
